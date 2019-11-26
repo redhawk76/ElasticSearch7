855d80465d9e81d792d320a6292d4ced1631cffb
redhawk <972419185@qq.com>
2019-11-26 11:23:24 +0800

    init

---
+++ 02-elasticsearch-springboot/src/test/java/xyz/redhawk/es/ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.redhawk.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
